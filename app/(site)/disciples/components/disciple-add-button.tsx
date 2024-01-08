b69e9d01da7bee8354d82807806f0ed51998375e
engrjeff <engineerjeff15@gmail.com>
2024-01-08 16:47:12 +0800

    better mobile ui

--- app/(site)/disciples/components/disciple-add-button.tsx
+++ app/(site)/disciples/components/disciple-add-button.tsx
@@ -13,16 +13,30 @@ function DiscipleAddButton({ leaderId }: { leaderId?: string }) {
     <>
       <Link
         href={{ pathname: "/disciples/new", query: { leaderId } }}
-        className={cn(buttonVariants({ size: "sm" }), "xl:hidden")}
+        className={cn(
+          buttonVariants({ size: "sm" }),
+          "hidden lg:flex xl:hidden"
+        )}
       >
         <Plus className="mr-3 h-4 w-4" />
         <span>Add</span>
       </Link>
 
+      <Link
+        href={{ pathname: "/disciples/new", query: { leaderId } }}
+        className={cn(
+          buttonVariants({ size: "icon" }),
+          "fixed bottom-4 right-4 z-30 flex h-14 w-14  shrink-0 rounded-full shadow-md lg:hidden"
+        )}
+      >
+        <Plus className="h-6 w-6" />
+        <span className="sr-only">Add</span>
+      </Link>
+
       <div className="hidden grow-0 divide-x divide-amber-500 self-start rounded-md xl:block">
         <Link
           href={{ pathname: "/disciples/new", query: { leaderId } }}
-          className={cn(buttonVariants({ size: "sm" }), "rounded-r-none")}
+          className={cn(buttonVariants(), "rounded-r-none")}
         >
           <Plus className="mr-3 h-4 w-4" />
           <span>Add</span>
--- components/site-header.tsx
+++ components/site-header.tsx
@@ -1,6 +1,7 @@
 import { ThemeToggle } from "@/components/theme-toggle"
 import UserMenu from "@/components/user-menu"
 
+import HeaderTitle from "./header-title"
 import MobileMenu from "./mobile-menu"
 import QuickActions from "./quick-actions"
 import UserGreeting from "./user-greeting"
@@ -8,15 +9,18 @@ import UserGreeting from "./user-greeting"
 export function SiteHeader() {
   return (
     <header className="sticky top-0 z-40 w-full border-b bg-background">
-      <div className="container flex h-16 items-center justify-between space-x-4">
+      <div className="container relative flex h-16 items-center justify-between gap-x-4">
         <div className="block lg:hidden">
           <MobileMenu />
         </div>
+        <HeaderTitle />
         <div className="hidden lg:block">
           <UserGreeting />
         </div>
         <div className="ml-auto flex items-center space-x-5">
-          <QuickActions />
+          <div className="hidden lg:block">
+            <QuickActions />
+          </div>
           <ThemeToggle />
           <UserMenu />
         </div>
